8adb324e7afed46e88ff46fa17c1127040e7b85f
D8991 <darkov.1998@mail.ru>
2018-05-25 14:52:40 +0300

    Добавление книги

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -17,17 +17,18 @@ namespace ConsoleApplication2
 
             Book good1 = new Book(book_Sale, "Преступление и наказание", 250, "Достоевский", "Роман", "Э");
             Book good2 = new Book(book_Sale, "Мастер и маргарита", 300, "Булгаков", "Роман", "Эксмо");
-
-            Journal good3 = new Journal(journal_Sale, "Наука", 150, new DateTime(2018, 02, 15));
-            Journal good4 = new Journal(journal_Sale, "Вестник", 140, new DateTime(2018, 03, 17));
+            Book good3 = new Book(book_Sale, "Бойцовский клуб", 200, "Паланик", "Роман", "Э");
+            Journal good4 = new Journal(journal_Sale, "Наука", 150, new DateTime(2018, 02, 15));
+            Journal good5 = new Journal(journal_Sale, "Вестник", 140, new DateTime(2018, 03, 17));
             Genre good6 = new Genre(book_Sale, "Десять негритят", 150, "Агата Кристи", "Э", genre_Sale);
 
             List<Good> ListGood = new List<Good>();
             ListGood.Add(good1);
             ListGood.Add(good2);
-            ListGood.Add(good6);
             ListGood.Add(good3);
             ListGood.Add(good4);
+            ListGood.Add(good5);
+            ListGood.Add(good6);
 
             string userChoise = Console.ReadLine();
             foreach (Good g in ListGood.Where(i1 => i1.Good_Sale.Sale_Name == userChoise))
